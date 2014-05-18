f31b627f52782521a86a9081f85851721cb29aa0
Boris <fierval@gmail.com>
2014-05-17 18:33:15 -0700

    kernels for sorting

--- HW4/student_func.cu
+++ HW4/student_func.cu
@@ -47,22 +47,83 @@
 #define RADIX (1 << NUM_BITS)
 #define BLOCK_SIZE 256 // no more than 254 in order to fit all relative positions into the array of bytes
 
+__device__ unsigned int get_digit(unsigned int num, unsigned int shift)
+{
+  int mask = (NUM_BITS - 1) << shift;
+  return (num & mask) >> shift;
+}
+
+__global__ void hist_prelim(const float* const in, const size_t size, unsigned int shift, const int nBins, unsigned int* const  outBins)
+{
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx >= size)
+  {
+    return;
+  }
+
+  unsigned int digit = get_digit(in[idx], shift);
+
+  atomicAdd(&outBins[digit], 1);
+}
+
+// exclusive scan sum of the histograms of digits
+__global__ void scan_sum_exclusive(const int * const in, const size_t size, unsigned int * const out, int * const sums)
+{
+  int idx = blockDim.x * blockIdx.x + threadIdx.x;
+
+  extern __shared__ int tempIn[];
+
+  tempIn[threadIdx.x] = threadIdx.x > 0 ? in[idx - 1] : 0;
+  __syncthreads();
+
+  for(int step = 1; step < size; step <<= 1)
+  {
+    __syncthreads();
+    int tSum = tempIn[threadIdx.x];
+
+    if(threadIdx.x >= step)
+    {
+      tSum += tempIn[threadIdx.x - step];
+    }
+    __syncthreads();
+    tempIn[threadIdx.x] = tSum;
+    
+  }
+
+  __syncthreads();
+  out[idx] = tempIn[threadIdx.x];
+  
+  __syncthreads();
+
+  int last = blockDim.x * blockIdx.x + size - 1;
+
+  if(threadIdx.x == 0)
+  {
+    sums[blockIdx.x] = out[last] + in[last];
+  }
+}
+
+// adding up partial sums to get the final output scan
+__global__ void addSums(unsigned int * const inOut, int * const vals)
+{
+  if(blockIdx.x == 0)
+  {
+    return;
+  }
+
+  int idx = blockDim.x * blockIdx.x + threadIdx.x;
+  inOut[idx] += vals[blockIdx.x - 1];
+}
+
 // Build a relative positional scan of digit, shifted by "shift"
 // in - array of numbers to sort
 // inPos - positions of the keys that also need to move
 // size - size of the array
 // shift - which significant digit are we concerned about
 // rel - array of relative positions of the numbers in the in array (relative to current block), based on the digit of interest.
-// startPos - array of relative start positions of each digit per block
-// out - output array. Everythng here is going to be sorted within the block at the end
-// outPos - output positions
-__global__ void sortRadixPerBlock(const unsigned int * const in, 
-                                     const unsigned int * const inPos, 
+__global__ void rel_pos_per_block(const unsigned int * const in, 
                                      const size_t size, unsigned char shift, 
-                                     unsigned char * const rel, 
-                                     unsigned int * const startPos, 
-                                     unsigned int * const out, 
-                                     unsigned int * const outPos)
+                                     unsigned char * const rel)
 {
   int idx = blockDim.x * blockIdx.x + threadIdx.x;
   unsigned int mask = (NUM_BITS - 1) << shift;
@@ -75,24 +136,10 @@ __global__ void sortRadixPerBlock(const unsigned int * const in,
   {
     relativePosition[threadIdx.x + i * BLOCK_SIZE] = 0;
   }
-  
-  // how many digits of each kind do we have?
-  for(int i = 0; i < RADIX; i++)
-  {
-    // remember how many of the digits of this kind we have encountered
-    if (i == digit_pos)
-    {
-      atomicAdd(&startPos[blockIdx.x + digit_pos], 1);
-      break;
-    }
-  }
   __syncthreads();
 
   // initialize to the "startup" positions for each digit.
-  if (digit_pos > 0)
-  {
-    relativePosition[threadIdx.x + digit_pos * BLOCK_SIZE] = threadIdx.x == 0 ? 0 : startPos[blockIdx.x + digit_pos - 1];
-  }
+  relativePosition[threadIdx.x + digit_pos * BLOCK_SIZE] = threadIdx.x == 0 ? 0 : 1;
 
   // accumulate position values for each digit
   for(int step = 1; step < size; step <<= 1)
@@ -111,59 +158,35 @@ __global__ void sortRadixPerBlock(const unsigned int * const in,
   }
 
   __syncthreads();
+  //combine relative positions with "start" positions
 
-  // now move things around
-  out[rel[idx]] = in[idx];
-  outPos[rel[idx]] = inPos[idx];
+  rel[idx] = relativePosition[threadIdx.x + offset];
+  __syncthreads();
 }
 
-// Convert relative postions to absolute ones by adding up all the histograms we now have in the
-// relPos array
-// n blocks of RADIX threads each
-// absPos, relPos - histograms of digit destribution
-// relPos - relative to each of the sorting blocks (n)
-// absPos - relative to the start of the array
-__global__ void relPosToAbsPos(const unsigned int * const relPos, unsigned int * const absPos)
+// Convert relative postions to absolute ones by adding up to the histogram
+// rel array of relative positions
+// cdf - exclusive cdf of the counts histogram
+__global__ void move_to_out(unsigned int * const dest, 
+                            unsigned int * const destPos, 
+                            unsigned int * const source, 
+                            unsigned int * const sourcePos, 
+                            unsigned int * const rel, 
+                            size_t size, 
+                            unsigned int shift, 
+                            unsigned int * const absPos, 
+                            unsigned int * const cdf)
 {
-  if (blockIdx.x == 0)
-  {
-    return;
-  }
-
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
-  int prevIdx = idx - blockDim.x; //(blockIdx.x - 1) * blockDim.x + threadIdx.x
-  
-  atomicAdd(&absPos[threadIdx.x], relPos[prevIdx]);
-}
-
-__global__ void moveToCorrectPos(unsigned int * const dest, 
-                                 const unsigned int * const source, 
-                                 size_t size,
-                                 unsigned int shift,
-                                 unsigned int * const destPos, 
-                                 const unsigned int * const sourcePos, 
-                                 const unsigned int * absPos,
-                                 const unsigned int * relPos)
-{
-  int idx = blockDim.x * blockIdx.x + threadIdx.x;
-
-  if (idx > size)
+  if(idx >= size)
   {
     return;
   }
 
-  unsigned int mask = (NUM_BITS - 1) << shift;
-  int digit = (source[idx] & mask) >> shift;
-
-  // moves numbers sorted within a block to their rightful place
-  // here threadIdx.x marks the right spot
-  int destIndex = absPos[digit] + relPos[blockIdx.x * RADIX + digit] + threadIdx.x;
-
-  dest[destIndex] = source[idx];
-  destPos[destIndex] = sourcePos[idx];
-  
+  int pos = cdf[get_digit(source[idx], shift)] + rel[idx];
+  dest[pos] = source[idx];
+  destPos[pos] = sourcePos[idx];
 }
-
 
 void your_sort(unsigned int* const d_inputVals,
                unsigned int* const d_inputPos,
